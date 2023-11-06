f49bbef9e7075bc2ca81f24489e757d4e389db1e
Robertas Brazinskas <S00238718@atu.ie>
2023-11-06 12:38:18 +0000

    alpha version

--- Blackjack CA1/Program.cs
+++ Blackjack CA1/Program.cs
@@ -9,18 +9,77 @@ namespace Blackjack_CA1
 {
     internal class Program
     {
+        //Global variables
         static int randomCard = 0;
-        static int firstNumber;
-        static int secondNumber;
+        static int playerNumber;
+        static int dealerNumber;
+        static int card;
+        static string spacing = new string('-', 35);
 
+        //Random declared here to have a different seed everytime
         static Random random = new Random();
         static void Main(string[] args)
         {
             while (true)
             {
-            GetFirstNumbers();
-            GetSecondNumbers();
-            CompareTwo();
+                Console.WriteLine(spacing);
+                //Get your cards
+                GetFirstNumbers();
+
+                //Asks for user input
+                Console.Write($"Do you want to hit or stand? (h/s): ");
+                string action = Console.ReadLine();
+
+                Console.WriteLine(spacing);
+
+                if (action == "h")
+                {
+                    int hitCard = GetAnotherCard();
+
+                    playerNumber += hitCard;
+
+                    if (playerNumber > 21)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{playerNumber}, You bust");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        //Get dealer cards
+                        GetSecondNumbers();
+
+                        if (action == "h")
+                        {
+                            int dealerHit = GetAnotherCard();
+                            Console.WriteLine($"Dealer hit {dealerHit}");
+
+                            dealerNumber += dealerHit;
+
+                            if (dealerNumber > 21)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine($"{dealerNumber}, Dealer bust");
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            else
+                            {
+                                //Compares your and dealer cards
+                                CompareCards();
+                            }
+                        }
+                    }
+                }
+
+                //Allows user to stop loop
+                Console.WriteLine($"{spacing}\nWould you like to go again? (y/n): ");
+
+                string goAgain = Console.ReadLine();
+
+                if (goAgain != "y")
+                {
+                    break;
+                }
             }
         }
 
@@ -28,53 +87,93 @@ namespace Blackjack_CA1
         {
             GetRandomCards();
 
-            Console.WriteLine($"First numbers are {randomCard}\n");
+            Console.WriteLine($"You got: {randomCard}");
 
-            firstNumber = randomCard;
+            playerNumber = randomCard;
 
-            return firstNumber;
+            return playerNumber;
         }
 
         static int GetSecondNumbers()
         {
             GetRandomCards();
 
-            Console.WriteLine($"Second numbers are {randomCard}\n");
+            Console.WriteLine($"Dealer got: {randomCard}");
 
-            secondNumber = randomCard;
+            dealerNumber = randomCard;
 
-            return secondNumber;
+            return dealerNumber;
         }
 
         static int GetRandomCards()
         {
-            int cardOne = random.Next(1, 10);
-            Console.WriteLine(cardOne);
-            int cardTwo = random.Next(1, 10);
-            Console.WriteLine(cardTwo);
+            int cardOne = RandomCard();
+            int cardTwo = RandomCard();
 
-            randomCard = cardOne + cardTwo; 
+            randomCard = cardOne + cardTwo;
 
             return randomCard;
         }
 
-        static bool CompareTwo()
+        static int RandomCard()
+        {
+            card = random.Next(1, 10);
+            
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            //Console.WriteLine($"RandomCard: {card}");
+            Console.ForegroundColor= ConsoleColor.White;
+            
+            return card;
+        }
+        
+
+        static bool CompareCards()
         {
 
-            if (firstNumber > secondNumber)
+            if (playerNumber > dealerNumber)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{firstNumber} is greater than {secondNumber}");
+                Console.WriteLine($"{playerNumber} is greater than {dealerNumber}");
                 Console.ForegroundColor = ConsoleColor.White;
                 return true;
             }
-            else
+            if (playerNumber < dealerNumber)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{firstNumber} is less than {secondNumber}");
+                Console.WriteLine($"{playerNumber} is less than {dealerNumber}");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{playerNumber} and {dealerNumber} are equal");
                 Console.ForegroundColor = ConsoleColor.White;
                 return false;
             }
         }
+        static int GetAnotherCard()
+        {
+            int hitCard = RandomCard();
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine($"Hit card: {hitCard}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            return hitCard;
+        }
     }
 }
+
+
+//Add class for cards
+
+//Comment
+
+//Add getting actuals cards
+
+//>21 Exception Loss
+
+//Add win/loss stats
+
+//Debugging 
