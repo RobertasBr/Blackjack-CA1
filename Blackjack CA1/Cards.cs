f95cee30e7b91f293181c506c985ecc86a2f5943
RobertasBr <123648407+RobertasBr@users.noreply.github.com>
2023-11-10 16:49:08 +0000

    Mostly finished BlackJack

---
+++ Blackjack CA1/Cards.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Blackjack_CA1
+{
+    class Deck
+    {
+        //Creates list of cards along with random number generator
+        private List<Card> cards = new List<Card>();
+        private Random random = new Random();
+
+        //All the card suits and ranks available
+        public Deck()
+        {
+            string[] suits = { "Hearts", "Diamonds", "Clubs", "Spades" };
+            string[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+
+            //Generetes 52 cards in a deck
+            foreach (string suit in suits)
+            {
+                foreach (string rank in ranks)
+                {
+                    cards.Add(new Card(rank, suit));
+                }
+            }
+        }
+
+        //Fisher-Yates shuffle algorithm
+        public void Shuffle()
+        {
+            int cardCount = cards.Count;
+            while (cardCount > 1)
+            {
+                cardCount--;
+                int randomNumber = random.Next(cardCount + 1);
+                Card value = cards[randomNumber];
+                cards[randomNumber] = cards[cardCount];
+                cards[cardCount] = value;
+            }
+        }
+
+        //Draws the card and removes it from current deck
+        public Card DrawCard()
+        {
+            Card card = cards[0];
+            cards.RemoveAt(0);
+            return card;
+        }
+    }
+    //Card class with ranks and suits as strings
+    class Card
+    {
+        public string Rank { get; }
+        public string Suit { get; }
+
+        public Card(string rank, string suit)
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+
+        //Gets values for A, K, Q, J
+        public int GetValue()
+        {
+            if (Rank == "Ace")
+                return 11;
+            else if (Rank == "King" || Rank == "Queen" || Rank == "Jack")
+                return 10;
+            else
+                return int.Parse(Rank);
+        }
+
+        //Allows hands to be displayed
+        public override string ToString()
+        {
+            return Rank + " of " + Suit;
+        }
+    }
+
+    //Player class used by player and dealer
+    class Player
+    {
+        private List<Card> hand = new List<Card>();
+
+        //Takes a card to player
+        public void AddCard(Card card)
+        {
+            hand.Add(card);
+        }
+
+        //Formats hand to existing string
+        public string GetHand()
+        {
+            return string.Join(", ", hand);
+        }
+
+        //Get dealer's first card thats visible
+        public string GetUpCard()
+        {
+            return hand[0].ToString();
+        }
+
+        //Gets values to decide win/loss/tie
+        public int GetScore()
+        {
+            int score = 0;
+            int numberOfAces = 0;
+
+            //Adds score of hand + checks for aces
+            foreach (Card card in hand)
+            {
+                score += card.GetValue();
+                if (card.Rank == "Ace")
+                    numberOfAces++;
+            }
+
+            //Handle Ace value
+            while (numberOfAces > 0 && score > 21)
+            {
+                score -= 10;
+                numberOfAces--;
+            }
+
+            return score;
+        }
+    }
+}
--- Blackjack CA1/Program.cs
+++ Blackjack CA1/Program.cs
@@ -1,24 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 
 namespace Blackjack_CA1
 {
     internal class Program
     {
         //Global variables
-        static int randomCard = 0;
-        static int playerNumber;
-        static int dealerNumber;
-        static int card;
-
         static int Wins = 0;
         static int Losses = 0;
         static int Ties = 0;
-        
+
         static string spacing = new string('-', 35);
 
         //Random declared here to have a different seed everytime
@@ -28,156 +18,108 @@ namespace Blackjack_CA1
             while (true)
             {
                 Console.WriteLine(spacing);
-                //Get your cards
-                GetFirstNumbers();
-
-                //Asks for user input
-                Console.Write($"Do you want to hit or stand? (h/s): ");
-                string action = Console.ReadLine();
-
+                PlayGame();
                 Console.WriteLine(spacing);
-
-                if (action == "h")
-                {
-                    int hitCard = GetAnotherCard();
-
-                    playerNumber += hitCard;
-
-                    if (playerNumber > 21)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"{playerNumber}, You bust");
-                        Console.ForegroundColor = ConsoleColor.White;
-                    }
-                    else
-                    {
-                        //Get dealer cards  //Replace with keep going until someone losses
-                        GetSecondNumbers();
-
-                        if (action == "h")
-                        {
-                            int dealerHit = GetAnotherCard();
-                            Console.WriteLine($"Dealer hit {dealerHit}");
-
-                            dealerNumber += dealerHit;
-
-                            if (dealerNumber > 21)
-                            {
-                                Console.ForegroundColor = ConsoleColor.Green;
-                                Console.WriteLine($"{dealerNumber}, Dealer bust");
-                                Console.ForegroundColor = ConsoleColor.White;
-                            }
-                            else
-                            {
-                                //Compares your and dealer cards //Replace with keep going until someone losses
-                                CompareCards();
-                            }
-                        }
-                    }
-                }
 
                 ShowStatistics();
 
-                //Allows user to stop loop
-                Console.WriteLine($"{spacing}\nWould you like to go again? (y/n): ");
+                Console.Write("Do you want to play again? (y/n): ");
+                if (Console.ReadLine().ToLower() == "n")
+                    break;
+            }
+        }
 
-                string goAgain = Console.ReadLine();
+        static void PlayGame()
+        {
+            //Shuffles deck on game start
+            Deck deck = new Deck();
+            deck.Shuffle();
 
-                if (goAgain != "y")
+            //Creates a player and dealer class
+            Player player = new Player();
+            Player dealer = new Player();
+
+            //Fairly gives cards out in alternating order
+            player.AddCard(deck.DrawCard());
+            dealer.AddCard(deck.DrawCard());
+            player.AddCard(deck.DrawCard());
+            dealer.AddCard(deck.DrawCard());
+
+            //Displays your hand and dealer up card
+            Console.WriteLine("Your cards: " + player.GetHand());
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Dealer's card: " + dealer.GetUpCard());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            while (true)
+            {
+                //Asks for user input
+                Console.Write("Do you want to hit or stand? (h/s): ");
+                string input = Console.ReadLine().ToLower();
+
+                if (input == "h")
+                {
+                    //Draws and displays card
+                    player.AddCard(deck.DrawCard());
+                    Console.WriteLine("Your cards: " + player.GetHand());
+
+                    //Early bust check
+                    if (player.GetScore() > 21)
+                    {
+                        Console.WriteLine("Bust! You lose.");
+                        Losses++;
+                        return;
+                    }
+                }
+                else if (input == "s")
                 {
                     break;
                 }
             }
-        }
 
-        static int GetFirstNumbers()
-        {
-            GetRandomCards();
-
-            Console.WriteLine($"You got: {randomCard}");
-
-            playerNumber = randomCard;
-
-            return playerNumber;
-        }
-
-        static int GetSecondNumbers()
-        {
-            GetRandomCards();
-
-            Console.WriteLine($"Dealer got: {randomCard}");
-
-            dealerNumber = randomCard;
-
-            return dealerNumber;
-        }
-
-        static int GetRandomCards()
-        {
-            int cardOne = RandomCard();
-            int cardTwo = RandomCard();
-
-            randomCard = cardOne + cardTwo;
-
-            return randomCard;
-        }
-
-        static int RandomCard()
-        {
-            card = random.Next(1, 10);
-            
-            Console.ForegroundColor = ConsoleColor.DarkGray;
-            //Console.WriteLine($"RandomCard: {card}");
-            Console.ForegroundColor= ConsoleColor.White;
-            
-            return card;
-        }
-        
-
-        static bool CompareCards()
-        {
-
-            if (playerNumber > dealerNumber)
+            //Makes the dealer 'decide' to hit or stand 
+            while (dealer.GetScore() < 17)
             {
-                //Win situation
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{playerNumber} is greater than {dealerNumber}");
-                Console.ForegroundColor = ConsoleColor.White;
-                Wins++;
-                return true;
+                dealer.AddCard(deck.DrawCard());
             }
-            if (playerNumber < dealerNumber)
+
+            Console.WriteLine("Your cards: " + player.GetHand());
+
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Dealer's cards: " + dealer.GetHand());
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //Determine the winner
+            int playerScore = player.GetScore();
+            int dealerScore = dealer.GetScore();
+
+            //Win/Loss/Tie conditions
+            if (playerScore > 21)
             {
-                //Loss situation
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{playerNumber} is less than {dealerNumber}");
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Bust! You lose.");
                 Losses++;
-                return false;
+            }
+            else if (dealerScore > 21 || playerScore > dealerScore)
+            {
+                Console.WriteLine("You win!");
+                Wins++;
+            }
+            else if (dealerScore > playerScore)
+            {
+                Console.WriteLine("You lose.");
+                Losses++;
             }
             else
             {
-                //Tie situation
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"{playerNumber} and {dealerNumber} are tied");
-                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("It's a tie!");
                 Ties++;
-                return false;
             }
         }
-        static int GetAnotherCard()
-        {
-            int hitCard = RandomCard();
 
-            //Shows what cards got hit
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine($"Hit card: {hitCard}\n");
-            Console.ForegroundColor = ConsoleColor.White;
-
-            return hitCard;
-        }
-        
-        static void ShowStatistics()
+        //Additional Feature: Show Statitics
+        //Keeps a tally of wins/losses/ties and displays after every match
+        public static void ShowStatistics()
         {
             Console.WriteLine($"\nYou currently have:");
             Console.ForegroundColor = ConsoleColor.Green;
@@ -185,15 +127,9 @@ namespace Blackjack_CA1
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"    Losses: {Losses}");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine($"    Ties: {Ties}");
+            Console.WriteLine($"    Ties: {Ties}\n");
             Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(spacing);
         }
     }
 }
-
-
-//Add class for cards
-
-//Add getting actuals cards
-
-//Debugging 
